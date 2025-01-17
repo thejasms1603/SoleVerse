088c4d34d768eed92b36d82bf21c64dcd2b1a4bd
thejasms1603 <thejasabhi705@gmail.com>
2025-01-17 14:28:46 -0500

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from './Footer';
+import SuperQuality from './SuperQuality';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+import SpecialOffers from './SpecialOffers';
+import Services from './Services';
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    Footer,
+    SpecialOffers,
+    Services,
+    SuperQuality,
+    PopularProducts,
+    Subscribe,
+    CustomerReviews,
+}
